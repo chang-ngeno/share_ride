37958e76c4896b0002730bb211c3fbd110eb1ba5
frank <franciskipchumba5@gmail.com>
2017-06-11 13:03:43 +0300

    fix asset path in pages

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
+<!doctype html>
 <html>
-<head>
-<title>Welcome</title>
-
-</head>
-    <body>
-    <?php
-    	require('pages/header.php');
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Book Ride</title>
+		<link rel="stylesheet" href="./assets/css/style.css" />
+	</head>
+	<body>
+		<?php
+			require('pages/header.php');
+		?>
+		<div> </div>
+		<div> <a href="ride/index.php">give a ride</a>
+		<br/> <a href="booking/index.php">get a ride</a>
+		<!-- <h3>Cancel a booked ride</h3>-->
+	</div>
+	<?php
+	require('pages/footer.php');
 	?>
-        <div> </div>
-        <div> <a href="ride/index.php">give a ride</a>
-            <br/> <a href="booking/index.php">get a ride</a>
-
-           <!-- <h3>Cancel a booked ride</h3>-->
-        </div>
-    </body>
-
-<?php
-    require('pages/footer.php');
-?>
-
+	<script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
+	<script src="js/nav-mobile.js"></script>
+	<script type="text/javascript">
+	</script>
+</body>
 </html>
--- ride/index.php
+++ ride/index.php
@@ -1,23 +1,30 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-	<title></title>
-
-</head>
-<body>
-<?php
-require("../pages/header.php");
-//require("sys.inc.php");
-?>
-<form action="reg_ride.php" method="POST">
-	Origin: <input type="text" name="origin"><br/>
-	destination:<input type="text" name="destination"><br/>
-	capacity:<input type="text" name="capacity"><br/>
-	<input type="submit" value="Submit">
-	<input type="reset" value="cancel">
-</form>
-<?php 
-require("../pages/footer.php");
-?>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Request Ride</title>
+		<link rel="stylesheet" href="../assets/css/style.css" />
+	</head>
+	<body>
+		<?php
+		require("../pages/header.php");
+		//require("sys.inc.php");
+		?>
+		<form action="reg_ride.php" method="POST">
+			Origin: <input type="text" name="origin"><br/>
+			destination:<input type="text" name="destination"><br/>
+			capacity:<input type="text" name="capacity"><br/>
+			<input type="submit" value="Submit">
+			<input type="reset" value="cancel">
+		</form>
+		<?php
+		require("../pages/footer.php");
+		?>
+		<script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
+		<script src="js/nav-mobile.js"></script>
+		<script type="text/javascript">
+		</script>
+	</body>
 </html>
