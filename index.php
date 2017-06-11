9ed182376d7ce9530a24ce4d528cf0ce90250dd0
frank <franciskipchumba5@gmail.com>
2017-06-11 12:53:26 +0300

    index start point

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Welcome</title>
+
+</head>
+    <body>
+    <?php
+    	require('pages/header.php');
+	?>
+        <div> </div>
+        <div> <a href="ride/index.php">give a ride</a>
+            <br/> <a href="booking/index.php">get a ride</a>
+
+           <!-- <h3>Cancel a booked ride</h3>-->
+        </div>
+    </body>
+
+<?php
+    require('pages/footer.php');
+?>
+
+</html>
