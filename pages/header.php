fa3def36cfeedf76d633c55fda03f13af259934b
frank <franciskipchumba5@gmail.com>
2017-06-11 12:54:35 +0300

    define templates

---
+++ pages/header.php
@@ -0,0 +1,23 @@
+<!-- <div class="header">
+	
+</div> -->
+<nav class="nav">
+
+        <a class="toggle-nav" href="#">&#9776;</a>
+
+        <div class="nav-mobile">
+            <ul class="left">
+                <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
+               <!--  <li><a href="#"><i class="fa fa-book"></i> Categories</a></li>
+                <li><a href="#"><i class="fa fa-fire"></i> Most Popular</a></li> -->
+            </ul>
+            <ul class="right">
+                <li>
+                    <a href="#"><i class="fa fa-user"></i>Login</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-envelope"></i>Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
