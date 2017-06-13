daf6c407f0aad7c2610b1016a51f83e21c4f8689
Daniel Chang'masa <danchangmasa@gmail.com>
2017-06-13 17:26:58 +0300

    Add files via upload

--- ride/book.php
+++ ride/book.php
@@ -4,14 +4,14 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Cancel Ride</title>
+		<title>Book Ride</title>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.4/foundation.min.css">
 		<link rel="stylesheet" href="../assets/css/style.css" />
 	</head>
 	<body>
 		<div class="row">
 			<?php
-			$sql_query="update set is_booked='Y' where ride_id='".$_POST['ride_id']."';";
+			$sql_query="update ride set is_booked='Y' where ride_id=".$_POST['ride'].";";
 			require("../config/config-db.php");
 			$con = new mysqli($host,$user,$passwd,$dbname);
 			if (!$con)
@@ -20,9 +20,9 @@
 			}else{
 				if ($con->query($sql_query) === TRUE) {
 				    echo "Ride booking successful";
-				    mail ($_POST['email'],"Confirmation for ride booking","This is to confirm that you have successfully booked a ride..");
+				    mail ($_POST['email'],"Confirmation for ride booking","This is to confirm that you have successfully booked a ride..","From: info@sharetide.inc" . "\r\n" );
 				} else {
-				    echo "Error booking a ride: " . $conn->error;
+				    echo "Error booking a ride: " . $con->error;
 				}
 				$con->close();
 			}
